07beab1e5d7d1ccb16a3a1366b6550e6ebf8af73
efrengtoledo <efrengtoledo@lewisu.edu>
2022-09-28 18:09:44 -0500

    Initial Commit

---
+++ GymBros/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GymBros
+//
+//  Created by Gabriel Toledo on 9/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GymBros/GymBrosApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymBrosApp.swift
+//  GymBros
+//
+//  Created by Gabriel Toledo on 9/28/22.
+//
+
+import SwiftUI
+
+@main
+struct GymBrosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
