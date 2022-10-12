f8715b84a99cf5c8338dc88976344ef5b9af89ad
efrengtoledo <efrengtoledo@lewisu.edu>
2022-10-12 13:01:45 -0500

    Added Simple Login and Register pages to the Navigation Hierarchy

--- GymBros/ContentView.swift
+++ GymBros/ContentView.swift
@@ -4,36 +4,42 @@
 //
 //  Created by Gabriel Toledo on 9/28/22.
 //
-
 import SwiftUI
+
+
+
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Color.black
-                .ignoresSafeArea()
-            VStack {
-                Text("GYMBROS")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.white)
-                    .padding()
-                    
-                Spacer()
-                Text("This is gonna be the logo asset")
-                    .foregroundColor(Color.white)
-                Spacer()
-                HStack {
+        NavigationStack {
+            ZStack {
+                Color.black
+                    .ignoresSafeArea()
+                VStack {
+                    Text("GYMBROS")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .padding()
                     Spacer()
-                    Text("LOGIN")
+                    Text("This is gonna be the logo asset")
                         .foregroundColor(Color.white)
                     Spacer()
-                    Text("JOIN")
-                        .foregroundColor(Color.white)
-                    Spacer()
+                    HStack {
+                        Spacer()
+                        NavigationLink(destination: LoginView(state: "LOG IN")) {
+                            Text("LOGIN")
+                                .foregroundColor(Color.white)
+                        }
+                          
+                        Spacer()
+                        NavigationLink(destination: RegisterView(state: "SIGN UP")) {
+                            Text("JOIN")
+                                .foregroundColor(Color.white)
+                        }
+                        Spacer()
+                    }
                 }
-                    
-                   
             }
         }
     }
---
+++ GymBros/Theme.swift
@@ -0,0 +1,10 @@
+//
+//  Theme.swift
+//  GymBros
+//
+//  Created by Gabriel Toledo on 10/12/22.
+//
+
+import SwiftUI
+
+//enum Theme: String {}
